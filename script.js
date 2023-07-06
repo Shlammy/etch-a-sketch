0fe2ef9cb6e755d22e0d3f74563a0ed97cdd530d
Shlammy <shlumhum@outlook.com>
2023-07-05 23:04:31 -0400

    + style, grid & mouseover, title

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const grid = document.querySelector('.gridContainer');
 
 
-for(let j =0; j < 16; j++){
-    const spaces = document.createElement('div');
-    spaces.classList.add('gridSquare');
-    grid.appendChild(spaces);
+for(let i = 0; i < 16; i++){
+    for(let j = 0; j < 16; j++){
+        const spaces = document.createElement('div');
+        spaces.classList.add('gridSquare');
+        spaces.addEventListener('mouseover', function(){spaces.style.backgroundColor = "black";});
+        grid.appendChild(spaces);
+    }
 }
 
 
