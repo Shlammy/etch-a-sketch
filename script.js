58a8aa46d970f45ad28b6d2f7e1f10e1f71fb8e7
Shlammy <shlumhum@outlook.com>
2023-07-10 13:45:15 -0400

    Documentation, adjusted function structure

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
+// Global Variables
 const grid = document.querySelector('.gridContainer');
 
-function resetPrompt() {
+// Loop to create Grid, Add classList, Event Listener
+function createGrid(){
+    for(let i = 0; i < 16; i++){
+        for(let j = 0; j < 16; j++){
+            const spaces = document.createElement('div');
+            spaces.classList.add('gridSquare');
+            spaces.addEventListener('mouseover', function(){spaces.style.backgroundColor = "black";});
+            grid.appendChild(spaces);
+        }
+    }
+}
+
+createGrid();
+
+// Reset Grid size to reflect user input.
+function resetGrid() {
     var userAnswer = prompt("How many squares per side? (Maximum 100)");
     return userAnswer;
 }
 
-for(let i = 0; i < 16; i++){
-    for(let j = 0; j < 16; j++){
-        const spaces = document.createElement('div');
-        spaces.classList.add('gridSquare');
-        spaces.addEventListener('mouseover', function(){spaces.style.backgroundColor = "black";});
-        grid.appendChild(spaces);
-    }
-}
+
 
 
 
