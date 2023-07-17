97b0627c4fe3f546d2d8b743ea4207c239943d14
Shlammy <shlumhum@outlook.com>
2023-07-16 23:14:41 -0400

    Adjusted structure of functions, + root var

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 // Global Variables
 const grid = document.querySelector('.gridContainer');
+const gridSquares = document.querySelectorAll(".gridSquare");
+let root = document.documentElement;
 
-// Loop to create Grid, Add classList, Event Listener
-function createGrid(){
-    for(let i = 0; i < 16; i++){
-        for(let j = 0; j < 16; j++){
-            const spaces = document.createElement('div');
-            spaces.classList.add('gridSquare');
-            spaces.addEventListener('mouseover', function(){spaces.style.backgroundColor = "black";});
-            grid.appendChild(spaces);
-        }
+// Loop to create Grid, Add classList, Event Listener, Initial Call for Grid
+function createGrid(numberOfSquares){
+    for(let i = 0; i < Math.pow(numberOfSquares, 2); i++){
+        const spaces = document.createElement('div');
+        spaces.classList.add('gridSquare');
+        grid.appendChild(spaces);
     }
 }
 
-createGrid();
+// Game initial call
+createGrid(16);
 
-// Reset Grid size to reflect user input.
+// Grid Space Color
+
+// Call prompt, reset grid size to prompt input
 function resetGrid() {
-    var userAnswer = prompt("How many squares per side? (Maximum 100)");
-    return userAnswer;
-}
+    var userAnswer = prompt("How many squares per side? (Min: 1 Max: 100)");
+    root.style.setProperty('--newNumberOfSquaresCss', userAnswer)
 
+    // Remove old grid & Call new grid
+    while (grid.firstChild) { 
+		grid.removeChild(grid.firstChild);
+	}
+
+    createGrid(userAnswer);
+}
 
 
 
